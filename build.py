646f3e3a7169c504b667cef731e4650c783c0cea
Sanyam Asthana <sanyam.asthana@gmail.com>
2025-10-28 17:47:42 +0530

    Testing Python Autobuild

---
+++ build.py
@@ -0,0 +1,96 @@
+import os
+
+with open("build.html", "w") as F:
+    pass
+
+
+def listfolders(directory, index):
+    if len(os.listdir(directory)) == 0:
+        with open("build.html", "a") as F:
+            F.write("</div>\n")
+        return
+
+    else:
+        for i in os.listdir(directory):
+            print(i)
+            flag = 0
+            if "." not in i:
+                print(f'"." is not in {i}, so it is a folder')
+                flag = 1
+                # print("  " * index + i)
+                with open("build.html", "a") as F:
+                    F.write(f"""
+                        <div class="h3-folder">
+                            <input
+                                type="checkbox"
+                                name="{i}"
+                                id="{i}"
+                                onchange="checkState(this)"
+                            />
+                            <label for="{i}">
+                                <span><b>{i}</b></span>
+                            </label>
+                                <div class="spanner">\n
+                        """)
+                s = directory + "/" + i
+                listfolders(s, index + 1)
+
+            if "." in i and i != ".DS_Store":
+                print(f'"." is in {i}, so it is a file')
+                with open("build.html", "a") as F:
+                    F.write(f"""
+                        <a href="{directory + "/" + i}"><h3>{i}</h3></a>
+                        """)
+            if flag:
+                with open("build.html", "a") as F:
+                    F.write("</div>\n" * (index))
+
+
+with open("build.html", "a") as F:
+    F.write("""
+        <!doctype html>
+        <html>
+            <head>
+                <title>2K25-Resources</title>
+                <link rel="stylesheet" href="styles.css" />
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            </head>
+
+            <body>
+                <div id="navbar">
+                    <a href="index.html">
+                        <h1>2K25-Resources</h1>
+                    </a>
+                    <div id="links">
+                        <a href="semester1.html">
+                            <h2 class="click-btn">Semester 1</h2>
+                        </a>
+                    </div>
+                </div>
+                <hr />\n
+        """)
+
+    F.flush()
+    listfolders("Semester-1", 1)
+
+    F.write("""
+        </div>
+    </div>
+            """)
+
+    F.write("""
+        </body>
+
+        <script>
+            function checkState(checkbox) {
+                const parentFolder = checkbox.parentElement;
+                const spanner = parentFolder.querySelector(".spanner");
+                if (checkbox.checked) {
+                    spanner.style.display = "block";
+                } else {
+                    spanner.style.display = "none";
+                }
+            }
+        </script>
+    </html>
+        """)
